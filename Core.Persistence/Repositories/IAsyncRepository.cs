f6a4b5ff789d811f593a62a88336b2c68aa39004
Samet Akca <smtakca99@gmail.com>
2024-08-05 17:22:11 +0300

    IQueryablePaginateExtensions and paginate

--- Core.Persistence/Repositories/IAsyncRepository.cs
+++ Core.Persistence/Repositories/IAsyncRepository.cs
@@ -14,26 +14,26 @@ public interface IAsyncRepository<TEntity, TEntityId> : IQuery<TEntity>
         CancellationToken cancellationToken = default);
 
 
-    //Task<Paginate<TEntity>> GetListAsync(
-    //    Expression<Func<TEntity, bool>>? predicate = null,
-    //    Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
-    //    Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
-    //    int index = 0,
-    //    int size = 10,
-    //    bool enableTracking = true,
-    //    CancellationToken cancellationToken = default
-    //);
+    Task<Paginate<TEntity>> GetListAsync(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        int index = 0,
+        int size = 10,
+        bool enableTracking = true,
+        CancellationToken cancellationToken = default
+    );
 
-    //Task<Paginate<TEntity>> GetListByDynamicAsync(
-    //    DynamicQuery dynamic,
-    //    Expression<Func<TEntity, bool>>? predicate = null,
-    //    Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
-    //    int index = 0,
-    //    int size = 10,
-    //    bool withDeleted = false,
-    //    bool enableTracking = true,
-    //    CancellationToken cancellationToken = default
-    //);
+    Task<Paginate<TEntity>> GetListByDynamicAsync(
+        DynamicQuery dynamic,
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        int index = 0,
+        int size = 10,
+        bool withDeleted = false,
+        bool enableTracking = true,
+        CancellationToken cancellationToken = default
+    );
 
     Task<bool> AnyAsync(
        Expression<Func<TEntity, bool>>? predicate = null,
