a574246a502968672e8ad63a005c8b798b667a04
Samet Akca <smtakca99@gmail.com>
2024-08-06 22:12:35 +0300

    EfRepositoryBase

--- Core.Persistence/Repositories/IAsyncRepository.cs
+++ Core.Persistence/Repositories/IAsyncRepository.cs
@@ -12,6 +12,7 @@ public interface IAsyncRepository<TEntity, TEntityId> : IQuery<TEntity>
     Task<TEntity> GetAsync(
         Expression<Func<TEntity, bool>> predicate,
         Func<IQueryable<TEntity>, IIncludableQueryable<TEntity ,object >>? include= null,
+        bool withDeleted = false,
         bool enableTracking = true,
         CancellationToken cancellationToken = default);
 
@@ -22,6 +23,7 @@ public interface IAsyncRepository<TEntity, TEntityId> : IQuery<TEntity>
         Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
         int index = 0,
         int size = 10,
+        bool withDeleted = false,
         bool enableTracking = true,
         CancellationToken cancellationToken = default
     );
